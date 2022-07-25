f03cf28bf31c6e7d16c120f1b6a5ae7ee0b9a229
Newton Lomar <newtonlomar16@gmail.com>
2022-07-25 08:40:32 -0300

    adding final js

--- script.js
+++ script.js
@@ -2,8 +2,43 @@ function criaCalculadora() {
   return {
     display: document.querySelector('.display'),
 
+    pressionaEnter() {
+      this.display.addEventListener('keyup', (e) => {
+        if (e.keyCode === 13) {
+          this.realizaConta();
+        }
+      });
+    },
+
+    realizaConta() {
+      let conta = this.display.value;
+
+      try {
+        conta = eval(conta); // perigoso
+
+        if (!conta) {
+          alert('Conta inválida!');
+          return;
+        }
+
+        this.display.value = String(conta);
+      } catch (e) {
+        alert('Conta inválida!');
+        return;
+      }
+    },
+
+    clearDisplay() {
+      this.display.value = '';
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.split('').slice(0, -1).join('');
+    },
+
     inicia() {
       this.cliqueBotoes();
+      this.pressionaEnter();
     },
 
     cliqueBotoes() {
@@ -14,6 +49,18 @@ function criaCalculadora() {
 
           if (el.classList.contains('btn-num')) {
             this.btnParaDisplay(el.innerText);
+          }
+
+          if (el.classList.contains('btn-clear')) {
+            this.clearDisplay();
+          }
+
+          if (el.classList.contains('btn-del')) {
+            this.apagaUm();
+          }
+
+          if (el.classList.contains('btn-eq')) {
+            this.realizaConta();
           }
         }.bind(this)
       );
